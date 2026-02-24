3f7dc2d2da72c4b85104a9669d1aebc4e5249c55
Collin Mickelson <57117254+Lavertas55@users.noreply.github.com>
2026-02-24 14:20:08 -0700

    refactored DataAccessException to extend DataException

--- server/src/main/java/dataaccess/exception/DataAccessException.java
+++ server/src/main/java/dataaccess/exception/DataAccessException.java
@@ -3,7 +3,7 @@ package dataaccess.exception;
 /**
  * Indicates there was an error connecting to the database
  */
-public class DataAccessException extends Exception{
+public class DataAccessException extends DataException{
     public DataAccessException(String message) {
         super(message);
     }
