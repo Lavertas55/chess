dfe72bd23dca5291038021faf8d12866edb9ebe2
Collin Mickelson <57117254+Lavertas55@users.noreply.github.com>
2026-02-26 17:42:49 -0700

    implemented user registration

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,16 +1,35 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryUserDAO;
+import exception.ResponseException;
 import io.javalin.*;
+import io.javalin.http.Context;
+import model.AuthData;
+import model.UserData;
+import request.LoginRequest;
+import service.AuthService;
+import service.UserService;
 
 public class Server {
 
     private final Javalin javalin;
+    private final AuthService authService;
+    private final UserService userService;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
-
-        // Register your endpoints and exception handlers here.
-
+        javalin = Javalin.create(config -> config.staticFiles.add("web"))
+                .delete("/db", this::clearData)
+                .post("/user", this::registerUser)
+                .post("/session", this::login)
+                .delete("/session", this::logout)
+                .get("/game", this::listGames)
+                .post("/game", this::createGame)
+                .put("/game", this::joinGame)
+                .exception(ResponseException.class, this::exceptionHandler);
+        authService = new AuthService(new MemoryAuthDAO());
+        userService = new UserService(new MemoryUserDAO());
     }
 
     public int run(int desiredPort) {
@@ -20,5 +39,44 @@ public class Server {
 
     public void stop() {
         javalin.stop();
+    }
+
+    private void clearData(Context ctx) {
+        throw new RuntimeException("not implemented");
+    }
+
+    private void registerUser(Context ctx) throws ResponseException {
+        UserData userData = new Gson().fromJson(ctx.body(), UserData.class);
+        String username = userService.register(userData);
+
+        AuthData authData = authService.generateSession(username);
+
+        ctx.result(authData.toJson());
+    }
+
+    private void login(Context ctx) throws ResponseException {
+        LoginRequest loginRequest = new Gson().fromJson(ctx.body(), LoginRequest.class);
+
+        userService.login(loginRequest.username(), loginRequest.password());
+    }
+
+    private void logout(Context ctx) {
+        throw new RuntimeException("not implemented");
+    }
+
+    private void listGames(Context ctx) {
+        throw new RuntimeException("not implemented");
+    }
+
+    private void createGame(Context ctx) {
+        throw new RuntimeException("not implemented");
+    }
+
+    private void joinGame(Context ctx) {
+        throw new RuntimeException("not implemented");
+    }
+
+    private void exceptionHandler(ResponseException e, Context ctx) {
+        throw new RuntimeException("not implemented");
     }
 }
