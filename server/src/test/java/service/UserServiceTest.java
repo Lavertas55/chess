26a4950487f431033de77e1be808469751e92a68
Collin Mickelson <57117254+Lavertas55@users.noreply.github.com>
2026-02-27 18:42:42 -0700

    removed unused exception

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -8,8 +8,6 @@ import model.UserData;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import request.LoginRequest;
-import request.RegisterRequest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
