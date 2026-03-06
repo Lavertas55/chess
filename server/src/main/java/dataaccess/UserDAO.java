4c2024296b09e3b36f849d41a3728435ef3764f9
Collin Mickelson <57117254+Lavertas55@users.noreply.github.com>
2026-03-05 17:34:16 -0700

    refactored UserDAO

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -2,8 +2,10 @@ package dataaccess;
 
 import dataaccess.exception.DataException;
 import model.UserData;
+import request.RegisterRequest;
 
 public interface UserDAO extends DataAccessObject {
-    void createUser(UserData userData) throws DataException;
+    UserData createUser(RegisterRequest registerRequest) throws DataException;
+    UserData getUser(int userID) throws DataException;
     UserData getUser(String username) throws DataException;
 }
