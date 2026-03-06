152ed3f63d3445fc453e0e2b9943e021ab843c12
Collin Mickelson <57117254+Lavertas55@users.noreply.github.com>
2026-03-05 17:32:47 -0700

    refactored MemoryUserDAO to use RegisterRequest to register user and updated map to store with userID as key

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -2,26 +2,42 @@ package dataaccess;
 
 import dataaccess.exception.*;
 import model.UserData;
+import org.eclipse.jetty.server.Authentication;
+import request.RegisterRequest;
 
 import java.util.HashMap;
 
 public class MemoryUserDAO implements UserDAO {
 
-    private final HashMap<String, UserData> userStorage = new HashMap<>();
+    private final HashMap<Integer, UserData> userStorage = new HashMap<>();
+    private final HashMap<String, Integer> usernameToID = new HashMap<>();
+    private int nextUserID = 1;
 
     @Override
-    public void createUser(UserData userData) throws DataException {
-        if (userData == null || userData.username() == null || userData.password() == null || userData.email() == null) {
+    public UserData createUser(RegisterRequest registerRequest) throws DataException {
+        if (registerRequest == null ||
+                registerRequest.username() == null ||
+                registerRequest.password() == null ||
+                registerRequest.email() == null
+        ) {
             throw new BadDataException("userData cannot be null.");
         }
 
-        String username = userData.username();
+        String username = registerRequest.username();
 
         if (hasUsername(username)) {
             throw new DataConflictException(String.format("username %s is already in use.", username));
         }
 
-        userStorage.put(username, userData);
+        String password = registerRequest.password();
+        String email = registerRequest.email();
+
+        UserData userData = new UserData(nextUserID++, username, password, email);
+
+        userStorage.put(userData.userID(), userData);
+        usernameToID.put(userData.username(), userData.userID());
+
+        return userData;
     }
 
     @Override
@@ -30,15 +46,27 @@ public class MemoryUserDAO implements UserDAO {
             throw new DataNotFoundException(String.format("username %s is not in use.", username));
         }
 
-        return userStorage.get(username);
+        int userID = usernameToID.get(username);
+        return getUser(userID);
+    }
+
+    @Override
+    public UserData getUser(int userID) throws DataException {
+        if (userStorage.containsKey(userID)) {
+            return userStorage.get(userID);
+        }
+        else {
+            throw new DataNotFoundException(String.format("userID %d does not exist", userID));
+        }
     }
 
     private boolean hasUsername(String username) {
-        return userStorage.containsKey(username);
+        return usernameToID.containsKey(username);
     }
 
     @Override
     public void clear() {
         userStorage.clear();
+        usernameToID.clear();
     }
 }
