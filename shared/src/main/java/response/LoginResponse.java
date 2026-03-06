e497757fd5d456d42a502038e8c39fb24f8f5528
Collin Mickelson <57117254+Lavertas55@users.noreply.github.com>
2026-03-05 17:35:54 -0700

    refactored LoginResponse to handle responding to login requests

--- shared/src/main/java/response/LoginResponse.java
+++ shared/src/main/java/response/LoginResponse.java
@@ -1,4 +1,9 @@
 package response;
 
+import com.google.gson.Gson;
+
 public record LoginResponse(String username, String authToken) {
+    public String toJson() {
+        return new Gson().toJson(this);
+    }
 }
