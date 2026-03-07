774b7c103dac5f43c2e8742e3c66783de27faef5
Collin Mickelson <57117254+Lavertas55@users.noreply.github.com>
2026-03-06 18:34:11 -0700

    refactored DatabaseManager to enforce unique usernames

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -17,7 +17,8 @@ public class DatabaseManager {
                 id INT PRIMARY KEY AUTO_INCREMENT,
                 username VARCHAR(255) NOT NULL,
                 password VARCHAR(255) NOT NULL,
-                email VARCHAR(255) NOT NULL
+                email VARCHAR(255) NOT NULL,
+                UNIQUE(username)
             )
             """,
             """
