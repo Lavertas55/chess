0472eb48dc59e72fff2841cbf8717149431bfd7c
Collin Mickelson <57117254+Lavertas55@users.noreply.github.com>
2026-03-07 19:52:54 -0700

    refactored MemoryGameDAOTest to use parameterized tests for memory and database implementations

--- server/src/test/java/dataaccess/MemoryGameDAOTest.java
+++ server/src/test/java/dataaccess/MemoryGameDAOTest.java
@@ -4,98 +4,175 @@ import chess.ChessGame;
 import dataaccess.exception.*;
 import model.GameData;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.*;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MemoryGameDAOTest {
 
-    private static MemoryGameDAO memoryDAO;
     private static GameData validGame;
+    private static int userID;
+
+    private static GameDAO getGameDAO(Class<? extends GameDAO> dbClass) throws DataException {
+        GameDAO gameDAO;
+        if (dbClass.equals(MySQLGameDAO.class)) {
+            DatabaseManager.createDatabase();
+            addUsers();
+            gameDAO = new MySQLGameDAO();
+        }
+        else {
+            gameDAO = new MemoryGameDAO();
+        }
+        gameDAO.clear();
+        return gameDAO;
+    }
+
+    private static void addUsers() throws DataException {
+        String statement = "INSERT INTO user (username, password, email) VALUES (\"user\", \"1234\", \"white@gmail.com\")";
+
+        try (var conn = DatabaseManager.getConnection()) {
+            var preparedStatement = conn.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.executeUpdate();
+
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            if (resultSet.next()) {
+                userID = resultSet.getInt(1);
+            }
+        }
+        catch (SQLException ex) {
+            throw new DataAccessException(String.format("failed to add users to database: %s", ex.getMessage()));
+        }
+    }
 
     @BeforeAll
     static void init() {
-        memoryDAO = new MemoryGameDAO();
-
-        String whiteUsername = null;
-        String blackUsername = null;
         String gameName = "game 1";
         String gameString = new ChessGame().getBoard().toJson();
 
-        validGame = new GameData(1, whiteUsername, blackUsername, gameName, gameString);
-    }
-
-    @BeforeEach
-    void setup() {
-        memoryDAO = new MemoryGameDAO();
-    }
-
-    @Test
-    void testCreateGame() throws DataException {
-        memoryDAO.createGame(validGame.gameName());
-
-        assertThrows(BadDataException.class, () -> memoryDAO.createGame(null));
-    }
-
-    @Test
-    void testGetGame() throws DataException {
-        GameData gameData = memoryDAO.createGame(validGame.gameName());
-
-        assertEquals(validGame, memoryDAO.getGame(gameData.gameID()));
-
-        assertThrows(DataNotFoundException.class, () -> memoryDAO.getGame(0));
-    }
-
-    @Test
-    void testListGames() throws DataException {
-        GameData gameData = memoryDAO.createGame(validGame.gameName());
-
-        Collection<GameData> gameList = new HashSet<>(Set.of(gameData));
-        assertEquals(gameList, memoryDAO.listGames());
+        validGame = new GameData(1, null, null, gameName, gameString);
     }
 
     @ParameterizedTest
-    @EnumSource(ChessGame.TeamColor.class)
-    void testUpdateUser(ChessGame.TeamColor teamColor) throws DataException {
-        GameData gameData = memoryDAO.createGame(validGame.gameName());
+    @ValueSource(classes = {MemoryGameDAO.class, MySQLGameDAO.class})
+    void testCreateGameValid(Class<? extends GameDAO> dbClass) throws DataException {
+        GameDAO gameDAO = getGameDAO(dbClass);
 
-        String newUsername = "new username";
-        memoryDAO.updateGameUser(gameData.gameID(), teamColor, newUsername);
+        assertDoesNotThrow(() -> gameDAO.createGame(validGame.gameName()));
+    }
 
-        GameData game = memoryDAO.getGame(gameData.gameID());
+    @ParameterizedTest
+    @ValueSource(classes = {MemoryGameDAO.class, MySQLGameDAO.class})
+    void testCreateGameNull(Class<? extends GameDAO> dbClass) throws DataException {
+        GameDAO gameDAO = getGameDAO(dbClass);
 
-        switch (teamColor) {
-            case WHITE -> assertEquals(newUsername, game.whiteUsername());
-            case BLACK -> assertEquals(newUsername, game.blackUsername());
+        assertThrows(BadDataException.class, () -> gameDAO.createGame(null));
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {MemoryGameDAO.class, MySQLGameDAO.class})
+    void testGetGameValid(Class<? extends GameDAO> dbClass) throws DataException {
+        GameDAO gameDAO = getGameDAO(dbClass);
+        GameData gameData = gameDAO.createGame(validGame.gameName());
+
+        assertEquals(validGame, gameDAO.getGame(gameData.gameID()));
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {MemoryGameDAO.class, MySQLGameDAO.class})
+    void testGetGameNonExistent(Class<? extends GameDAO> dbClass) throws DataException {
+        GameDAO gameDAO = getGameDAO(dbClass);
+
+        assertThrows(DataNotFoundException.class, () -> gameDAO.getGame(0));
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {MemoryGameDAO.class, MySQLGameDAO.class})
+    void testListGames(Class<? extends GameDAO> dbClass) throws DataException {
+        GameDAO gameDAO = getGameDAO(dbClass);
+
+        GameData gameData = gameDAO.createGame(validGame.gameName());
+        Collection<GameData> gameList = new HashSet<>(Set.of(gameData));
+
+        assertEquals(gameList, gameDAO.listGames());
+    }
+
+    static class TestUpdateUser {
+
+        @ParameterizedTest
+        @MethodSource("getParameterizedStream")
+        void valid(Class<? extends GameDAO> dbClass, ChessGame.TeamColor teamColor) throws DataException {
+            GameDAO gameDAO = getGameDAO(dbClass);
+
+            GameData gameData = gameDAO.createGame(validGame.gameName());
+
+            gameDAO.updateGameUser(gameData.gameID(), teamColor, userID);
+
+            GameData game = gameDAO.getGame(gameData.gameID());
+
+            switch (teamColor) {
+                case WHITE -> assertEquals(userID, game.whiteUserID());
+                case BLACK -> assertEquals(userID, game.blackUserID());
+            }
         }
 
-        assertThrows(DataNotFoundException.class, () -> memoryDAO.updateGameUser(0, teamColor, newUsername));
+        @ParameterizedTest
+        @MethodSource("getParameterizedStream")
+        void notFound(Class<? extends GameDAO> dbClass, ChessGame.TeamColor teamColor) throws DataException {
+            GameDAO gameDAO = getGameDAO(dbClass);
+
+            assertThrows(DataNotFoundException.class, () -> gameDAO.updateGameUser(0, teamColor, userID));
+        }
+
+        static Stream<Arguments> getParameterizedStream() {
+            List<Class<? extends GameDAO>> classes = List.of(MemoryGameDAO.class, MySQLGameDAO.class);
+
+            return Arrays.stream(ChessGame.TeamColor.values())
+                    .flatMap(teamColor ->
+                        classes.stream().map(dbClass -> Arguments.of(dbClass, teamColor))
+                    );
+        }
     }
 
-    @Test
-    void testUpdateGameString() throws DataException {
-        GameData gameData = memoryDAO.createGame(validGame.gameName());
+    @ParameterizedTest
+    @ValueSource(classes = {MemoryGameDAO.class, MySQLGameDAO.class})
+    void testUpdateGameStringValid(Class<? extends GameDAO> dbClass) throws DataException {
+        GameDAO gameDAO = getGameDAO(dbClass);
+
+        GameData gameData = gameDAO.createGame(validGame.gameName());
 
         String newGameString = "new game string";
-        memoryDAO.updateGameString(gameData.gameID(), newGameString);
+        gameDAO.updateGameString(gameData.gameID(), newGameString);
 
-        GameData game = memoryDAO.getGame(gameData.gameID());
+        GameData game = gameDAO.getGame(gameData.gameID());
         assertEquals(newGameString, game.gameString());
-
-        assertThrows(DataNotFoundException.class, () -> memoryDAO.updateGameString(0, newGameString));
     }
 
-    @Test
-    void testClear() throws DataException {
-        memoryDAO.createGame(validGame.gameName());
+    @ParameterizedTest
+    @ValueSource(classes = {MemoryGameDAO.class, MySQLGameDAO.class})
+    void testUpdateGameStringNotFound(Class<? extends GameDAO> dbClass) throws DataException {
+        GameDAO gameDAO = getGameDAO(dbClass);
 
-        memoryDAO.clear();
+        assertThrows(DataNotFoundException.class, () -> gameDAO.updateGameString(0, "test"));
+    }
 
-        assertThrows(DataNotFoundException.class, () -> memoryDAO.getGame(validGame.gameID()));
+    @ParameterizedTest
+    @ValueSource(classes = {MemoryGameDAO.class, MySQLGameDAO.class})
+    void testClear(Class<? extends GameDAO> dbClass) throws DataException {
+        GameDAO gameDAO = getGameDAO(dbClass);
+
+        gameDAO.createGame(validGame.gameName());
+
+        gameDAO.clear();
+
+        assertThrows(DataNotFoundException.class, () -> gameDAO.getGame(validGame.gameID()));
     }
 }
