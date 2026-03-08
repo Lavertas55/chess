2d68120c73a6fcc5fa371e591ccc19a133f85db0
Collin Mickelson <57117254+Lavertas55@users.noreply.github.com>
2026-03-07 19:59:07 -0700

    renamed MemoryGameDAOTest to GameDAOTest

--- server/src/test/java/dataaccess/GameDAOTest.java
+++ server/src/test/java/dataaccess/GameDAOTest.java
@@ -17,7 +17,7 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class MemoryGameDAOTest {
+public class GameDAOTest {
 
     private static GameData validGame;
     private static int userID;
