24c2761a1e29f6f811ada8af5208cabb4994e7e3
Collin Mickelson <57117254+Lavertas55@users.noreply.github.com>
2026-03-17 22:14:29 -0600

    added tests for ServerFacade::register

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,34 @@
 package client;
 
+import exception.ResponseException;
 import org.junit.jupiter.api.*;
 import server.Server;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+
+        facade = new ServerFacade(String.format("http://localhost:%d", port));
+    }
+
+    @AfterEach
+    public void cleanup() {
+        try {
+            facade.clear();
+        }
+        catch (ResponseException ex) {
+            fail(String.format("Failed to clear database: %s", ex.getMessage()));
+        }
     }
 
     @AfterAll
@@ -22,8 +38,25 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void registerValid() {
+        var authData = assertDoesNotThrow(() -> facade.register(
+                "player1",
+                "password",
+                "player1@email.com"
+        ));
+
+        assertTrue(authData.authToken().length() > 10);
+    }
+
+    @Test
+    public void registerNull() {
+        ResponseException exception = assertThrows(ResponseException.class, () -> facade.register(
+                null,
+                null,
+                null
+        ));
+
+        assertEquals(ResponseException.Code.BAD_REQUEST, exception.getCode());
     }
 
 }
