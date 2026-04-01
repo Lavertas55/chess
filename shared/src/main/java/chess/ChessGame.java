8e6734bd05f1cdff5d118dda992498c612e50a62
Collin Mickelson <57117254+Lavertas55@users.noreply.github.com>
2026-03-31 19:21:55 -0600

    implemented JSON conversion of ChessGame

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,5 +1,7 @@
 package chess;
 
+import com.google.gson.Gson;
+
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
@@ -20,6 +22,10 @@ public class ChessGame implements Cloneable {
 
         board = new ChessBoard();
         board.resetBoard();
+    }
+
+    public static ChessGame fromJson(String json) {
+        return new Gson().fromJson(json, ChessGame.class);
     }
 
     /**
@@ -215,6 +221,10 @@ public class ChessGame implements Cloneable {
      */
     public ChessBoard getBoard() {
         return board;
+    }
+
+    public String toJson() {
+        return new Gson().toJson(this);
     }
 
     @Override
