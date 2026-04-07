f2e9a593a5ac6925c1d7cc66213706fbe74c7823
Collin Mickelson <57117254+Lavertas55@users.noreply.github.com>
2026-04-07 11:33:19 -0600

    refactored registration of notification handler

--- client/src/main/java/client/websocket/WebSocketFacade.java
+++ client/src/main/java/client/websocket/WebSocketFacade.java
@@ -3,6 +3,7 @@ package client.websocket;
 import com.google.gson.Gson;
 import exception.ResponseException;
 import jakarta.websocket.*;
+import websocket.commands.UserGameCommand;
 import websocket.messages.ServerMessage;
 
 import java.io.IOException;
@@ -10,22 +11,17 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 public class WebSocketFacade extends Endpoint {
-    Session session;
-    NotificationHandler webSocketNotificationHandler;
+    final Session session;
+    final NotificationHandler notificationHandler;
 
-    public WebSocketFacade(String url, NotificationHandler webSocketNotificationHandler) throws ResponseException {
+    public WebSocketFacade(String url, NotificationHandler notificationHandler) throws ResponseException {
         try {
             url = url.replace("http", "ws");
             URI socketURI = new URI(url + "/ws");
-            this.webSocketNotificationHandler = webSocketNotificationHandler;
+            this.notificationHandler = notificationHandler;
 
             WebSocketContainer container = ContainerProvider.getWebSocketContainer();
             this.session = container.connectToServer(this, socketURI);
-
-            this.session.addMessageHandler((MessageHandler.Whole<String>) message -> {
-                ServerMessage notification = new Gson().fromJson(message, ServerMessage.class);
-                webSocketNotificationHandler.handleNotification(notification);
-            });
         }
         catch (DeploymentException | IOException | URISyntaxException ex) {
             throw new ResponseException(ResponseException.Code.SERVER_ERROR, ex.getMessage());
@@ -34,5 +30,23 @@ public class WebSocketFacade extends Endpoint {
 
     @Override
     public void onOpen(Session session, EndpointConfig endpointConfig) {
+        session.addMessageHandler(new MessageHandler.Whole<String>() {
+            @Override
+            public void onMessage(String message) {
+                ServerMessage notification = new Gson().fromJson(message, ServerMessage.class);
+                notificationHandler.handleNotification(notification);
+            }
+        });
+    }
+
+    public void joinGame(String authToken, int gameID) throws ResponseException {
+        UserGameCommand command = new UserGameCommand(UserGameCommand.CommandType.CONNECT, authToken, gameID);
+
+        try {
+            session.getBasicRemote().sendText(new Gson().toJson(command));
+        }
+        catch (IOException ex) {
+            throw new ResponseException(ResponseException.Code.SERVER_ERROR, ex.getMessage());
+        }
     }
 }
